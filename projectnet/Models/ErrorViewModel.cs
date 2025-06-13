35f720ca75e7d528f8398d7c9bdd9f38c6d677a8
unknown <hdang4747@gmail.com>
2025-06-13 08:42:25 +0700

    1st commit

---
+++ projectnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projectnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
